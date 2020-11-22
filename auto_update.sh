00781688be8b59f46e9dc449b4225421302a7ea1
Brian Blankenship <brianblankenship1978@gmail.com>
2020-11-21 23:57:37 -0500

    Fixy fix

--- auto_update.sh
+++ auto_update.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 # Linux (APT Package Control) Auto Update
+# Add a cronjob for me.
 
+which apt &>/dev/null; [[ $? == 0 ]] || { echo -ne "\n\# APT not installed\n"; };
+
+echo -ne "__________________________________\nUpdating APT Database\n"
+apt update && { echo "Update Complete" } || { echo "Update Failed, see error.\n"; exit 1; };
+echo -ne "__________________________________\nRunning Autoclean on local packages\n"
+apt autoclean -y && { echo "Autoclean Complete" } || { echo "Autoclean Failed, see error."; exit 1; };
+echo -ne "__________________________________\nUpgradeing Packages\n"
+apt full-upgrade -y && { echo "Package Upgrade Complete" } || { echo "Package Upgrade Failed, see error."; exit 1; };
+echo -ne "__________________________________\nRunning Autoremove on local packages\n"
+apt autoremove -y && { echo "Autoremove Complete" } || { echo "Autoremove Failed, see error."; exit 1; };
 echo "__________________________________"
-echo "Updating APT Database"
-apt update && echo "Update Complete" || echo "Update Failed, see error."
-echo "__________________________________"
-echo "Running Autoclean APT Database"
-apt autoclean -y && echo "Autoclean Complete" || echo "Autoclean Failed, see error."
-echo "__________________________________"
-echo "Running Autoremove APT Database"
-apt autoremove -y && echo "Autoremove Complete" || echo "Autoremove Failed, see error."
-echo "__________________________________"
-echo "Upgradeing Packages"
-apt upgrade -y && echo "Package Upgrade Complete" || echo "Package Upgrade Failed, see error."
-echo "__________________________________"
+
+exit 0;
